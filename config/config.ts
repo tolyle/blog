c65fdba14fb5020af113357e203c346fb1883f71
lyle <tolyle@outlook.com>
2023-03-26 14:37:39 +0800

    基本结构

--- config/config.ts
+++ config/config.ts
@@ -2,11 +2,15 @@ import { defineConfig } from "umi";
 import routes from '../routes'
 
 export default defineConfig({
+  favicons: [
+    'https://img.alicdn.com/tfs/TB1YHEpwUT1gK0jSZFhXXaAtVXa-28-27.svg',
+  ],
   plugins: ['@umijs/plugins/dist/antd'],
   
   routes: routes,
   npmClient: 'pnpm',
   antd:{
     dark:false
-  }
+  },
+  
 });
